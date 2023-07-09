ff33a6f1d0bb25e7c997060f6f5c2832e144d9b8
HEKUCHAN <heitorhirose@gmail.com>
2023-07-10 00:33:02 +0900

    通話の接続、切断

--- gachiniii/commands/voice_chat/join.py
+++ gachiniii/commands/voice_chat/join.py
@@ -6,4 +6,11 @@ from gachiniii import discord_bot
 @discord_bot.tree.command(name="vc-join", description="ボイスチャットに接続させる")
 async def vc_join(interaction: Interaction):
     logger.info("Executed vc-join command")
-    await interaction.response.send_message("仕方ねぇーな？？？")
+
+    if interaction.user.voice is None:
+        await interaction.response.send_message("は？通話入ってから呼べよ、くそが")
+    elif interaction.guild.voice_client is None:
+        await interaction.response.send_message("これでいいか？")
+        await interaction.user.voice.channel.connect()
+    else:
+        await interaction.response.send_message("仕方ねぇーな？？？")
--- gachiniii/commands/voice_chat/leave.py
+++ gachiniii/commands/voice_chat/leave.py
@@ -6,4 +6,8 @@ from gachiniii import discord_bot
 @discord_bot.tree.command(name="vc-leave", description="ボイスチャットから切断させる")
 async def vc_leave(interaction: Interaction):
     logger.info("Executed vc-leave command")
-    await interaction.response.send_message("だるくなった、抜けるわ")
+    if interaction.guild.voice_client is None:
+        await interaction.response.send_message("おれ通話入ってないけど？頭大丈夫？")
+    else:
+        await interaction.guild.voice_client.disconnect()
+        await interaction.response.send_message("やっと終わったわぁ、パチンコ行ってくるから呼ぶなよ")
