5e04bf984406e160810e2ea87537651dc74a8d7f
HEKUCHAN <heitorhirose@gmail.com>
2023-07-09 10:00:53 +0900

    add minimal discord bot functions.

---
+++ src/config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_API_TOKEN = os.getenv('DISCORD_API_TOKEN')
+LOGURU_FILTER_TYPE = os.getenv('LOGURU_FILTER_TYPE')
+
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from gachiniii import Gachiniii
+from config import DISCORD_API_TOKEN
+
+if __name__ == '__main__':
+    discord_bot = Gachiniii(
+        token=DISCORD_API_TOKEN
+    )
+    discord_bot.run()
