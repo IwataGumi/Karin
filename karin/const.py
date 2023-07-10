d9f7138e6b8dae07598d321c3a7b9af5cdf74f7b
HEKUCHAN <heitorhirose@gmail.com>
2023-07-11 08:49:26 +0900

    config.py to const.py

--- karin/const.py
+++ karin/const.py
@@ -8,6 +8,14 @@ DISCORD_BOT_NAME = os.getenv("DISCORD_BOT_NAME")
 DISCORD_API_TOKEN = os.getenv("DISCORD_API_TOKEN")
 LOGURU_FILTER_TYPE = os.getenv("LOGURU_FILTER_TYPE")
 DEFAULT_BOT_LANGUAGE = os.getenv("DEFAULT_BOT_LANGUAGE")
+DEFAULT_BOT_CONFIG = {}
+# Check here languages are available in discord.py
+# https://discordpy.readthedocs.io/en/stable/api.html?highlight=locale#discord.Locale
+SUPPORTED_LANGUAGES_WITH_EMOJI = {
+    "en-US": "🇺🇸",
+    "ja": "🇯🇵",
+}
+SUPPORTED_LANGUAGES = [lang for lang in SUPPORTED_LANGUAGES_WITH_EMOJI.keys()]
 
 # PostgreSQL
 POSTGRES_DRIVER = os.getenv("POSTGRES_DRIVER")
